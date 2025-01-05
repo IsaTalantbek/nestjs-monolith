42eedca9b67cf3a8ab7ecacd0849c5d8c2ee91a9
IsaT <isatalantbek@gmail.com>
2025-01-05 17:40:09 +0600

    micro-2.1.3-05.01.25-Добавил локальный редис, хз как его настраивать. А еще добавил лимит запросов для каждого человека

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import {
 import cookie from '@fastify/cookie'
 import { ConfigService } from '@nestjs/config'
 import { ValidationPipe } from '@nestjs/common'
+import fastifyRateLimit from 'fastify-rate-limit'
+import { Redis } from 'ioredis'
 
 async function bootstrap() {
     // Создаем приложение с использованием Fastify
@@ -15,6 +17,17 @@ async function bootstrap() {
         new FastifyAdapter()
     )
 
+    const redis = new Redis({
+        host: 'localhost',
+        port: 6379, // Проверьте конфигурацию вашего Redis
+    })
+
+    app.register(fastifyRateLimit, {
+        max: 100, // максимальное количество запросов
+        timeWindow: '1 minute', // за одну минуту
+        keyGenerator: (req) => req.ip, // Идентификатор пользователя (по IP)
+        redis,
+    })
     // Получаем ConfigService из контейнера зависимостей NestJS
     const configService = app.get(ConfigService)
 
