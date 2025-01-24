d3c054fd3f7104bce5d5264443ca8483f6016b8f
Isat <isatalantbek@gmail.com>
2025-01-24 21:15:21 +0600

    documentation-0.3.2-24.01.25-Небольшие изменения. А еще исправил баг в логгер декораторе

--- src/common/decorators/logger.decorator.ts
+++ src/common/decorators/logger.decorator.ts
@@ -4,9 +4,7 @@ export const LOGGING_FILE = 'logging_file'
 
 // Дает метаданные для глобального интерсептора логгирования, а именно название файла для записи
 // Если нету названия, то по дефолту записывает только ошибки+
-export function Log(
-    fileName: string = `${process.env.DEFAULT_LOG_FILE}/errors.log`
-) {
+export function Log(fileName: string = `errors`) {
     return SetMetadata(
         LOGGING_FILE,
         `${process.env.DEFAULT_LOG_FILE}/${fileName}.log`
